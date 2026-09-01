f2d26baba3de019065cabb0327092d6bc03476c1
Vasyl Molnar <vasyl.molnar@getwellis.com>
2026-09-01 11:40:25 +0000

    Changed data sources

--- definitions/sources/calendly.js
+++ definitions/sources/calendly.js
@@ -1,39 +1,39 @@
-declare_source({
+declare({
   database: "wellis-400118",
   schema: "calendly",
   name: "users",
   description: "Raw users (employees) table"
 });
 
-declare_source({
+declare({
   database: "wellis-400118",
   schema: "calendly",
   name: "event",
   description: "Raw events/meetings table"
 });
 
-declare_source({
+declare({
   database: "wellis-400118",
   schema: "calendly",
   name: "event_type",
   description: "Raw event type templates table"
 });
 
-declare_source({
+declare({
   database: "wellis-400118",
   schema: "calendly",
   name: "user_availability_schedule",
   description: "Raw availability schedule headers"
 });
 
-declare_source({
+declare({
   database: "wellis-400118",
   schema: "calendly",
   name: "user_availability_schedule_rule",
   description: "Raw date-level or weekday-level scheduling rules"
 });
 
-declare_source({
+declare({
   database: "wellis-400118",
   schema: "calendly",
   name: "user_availability_schedule_rule_interval",
